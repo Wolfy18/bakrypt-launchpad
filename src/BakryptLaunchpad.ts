f314d0904d6f6c108f16d59245043d7248bd0c4d
Wolf <wolfgangleon91@gmail.com>
2022-05-28 14:13:02 -0400

    Added attribute handlers to pass the access and refresh token

--- src/BakryptLaunchpad.ts
+++ src/BakryptLaunchpad.ts
@@ -1,12 +1,12 @@
 import { css } from 'lit';
-import { html, component, useEffect } from 'haunted';
+import { html, component, useEffect, useState } from 'haunted';
 import shoeStyles from '@shoelace-style/shoelace/dist/themes/light.styles';
 import { style } from './assets/css/main.css';
 import { gridStyles } from './assets/css/grid.css';
 import { useStyles } from './hooks/useStyles';
 import { BasicComponent } from './components/tab';
-// import '@shoelace-style/shoelace/dist/components/button/button';
-// import '@shoelace-style/shoelace/dist/components/button-group/button-group';
+import '@shoelace-style/shoelace/dist/components/button/button';
+import '@shoelace-style/shoelace/dist/components/button-group/button-group';
 
 window.customElements.define('bk-tab', component(BasicComponent));
 
@@ -23,6 +23,9 @@ function BakryptLaunchpad(this: any) {
     `,
   ]);
 
+  const [accessToken, setAccessToken] = useState();
+  const [refreshToken, setRefreshToken] = useState();
+
   useEffect(async () => {
     console.log('alerrt!!');
     for (let i = 0; i <= 5; i += 1) {
@@ -33,6 +36,16 @@ function BakryptLaunchpad(this: any) {
       this.shadowRoot.querySelector('.tab-container').appendChild(bkTab);
       console.log('added! -----');
     }
+
+    const _access = this.getAttribute('access-token');
+    if (_access) {
+      setAccessToken(_access);
+    }
+
+    const _refresh = this.getAttribute('refresh-token');
+    if (_refresh) {
+      setRefreshToken(_refresh);
+    }
   }, []);
 
   return html`
@@ -42,10 +55,10 @@ function BakryptLaunchpad(this: any) {
         <bk-tab .index=${0}></bk-tab>
       </div>
     </div>
-    <!-- <sl-button-group>
+    <sl-button-group>
       <sl-button>Left</sl-button>
       <sl-button>Right</sl-button>
-    </sl-button-group> -->
+    </sl-button-group>
   `;
 }
 
